5f8fbf7ebbbb7fa17fa6e145576abe8cf380ba12
AKASH U <akashu97@gmail.com>
2025-01-06 19:36:11 +0000

    typeconversion

---
+++ basics/02_datatyes.js
@@ -0,0 +1,17 @@
+let score = "33abs";
+
+let scoreInNum = Number(score)
+console.log(scoreInNum)
+console.log(typeof(scoreInNum))
+
+//let isLogged = 0
+//console.log(Boolean(isLogged))
+
+
+//Boolean of empty striing is false
+//Boolean of string is true
+//Boolean of 1 is true and 0 is false
+let isLogged = "abc"
+console.log(Boolean(isLogged))
+
+
